13a7906374423c32df06924082d384ae8ce08677
Felipe Espinoza <j.felipe.espinozah@gmail.com>
2026-03-23 22:42:50 -0500

    cambio de drawer a modal

--- src/pages/employees/panels/CreateEmployee.tsx
+++ src/pages/employees/panels/CreateEmployee.tsx
@@ -1,8 +1,9 @@
 import {
   ComboBox,
-  Drawer,
+  DatePicker,
   DropZone,
   Input,
+  Modal,
   type IComboBoxOption,
 } from "arise-ui";
 
@@ -19,44 +20,37 @@ export const CreateEmployee = ({ isOpen, onClose }: ICreateEmployeeProps) => {
   ];
 
   return (
-    <Drawer
+    <Modal
       title="Create User"
       description="Register a new user and assign them to a role."
-      primaryButtonLabel="Create"
       isOpen={isOpen}
       onClose={onClose}
-      onSuccess={() => console.log("User created!")}
     >
-      <span className="mb-2 inline-block text-xs font-medium text-neutral-600 uppercase">
-        Image Profile
-      </span>
-      <div className="h-44">
-        <DropZone />
-      </div>
-      <span className="my-3 inline-block text-xs font-medium text-neutral-600 uppercase">
+      <span className="mb-3 inline-block text-xs font-medium text-neutral-600 uppercase">
         Personal Information
       </span>
-      <div className="mb-2 flex flex-col gap-2">
-        <Input title="Name" status="error"/>
-        <Input title="Last Name" />
+      <div className="flex justify-between gap-3">
+        <div className="inline-block h-45 w-[50%]">
+          <DropZone />
+        </div>
+        <div className="flex w-[50%] flex-col gap-[5.5px]">
+          <Input title="Name" />
+          <Input title="Last Name" />
+          <Input title="Dni" />
+        </div>
       </div>
-      <div className="grid grid-cols-2 gap-2">
-        <Input title="Dni" />
+      <div className="mt-2 grid grid-cols-2 gap-2">
         <Input title="Phone" />
-      </div>
-      <div className="mt-2">
         <ComboBox options={genderOptions} title="Gender" />
       </div>
       <span className="my-3 inline-block text-xs font-medium text-neutral-600 uppercase">
         Employment information
       </span>
       <div className="mb-2 flex flex-col gap-2">
-        <Input title="Name" />
-        <Input title="Last Name" />
+        <DatePicker title="Birthday" />
+        <Input title="Code" />
+        {/* <ComboBox options={genderOptions} title="User" /> */}
       </div>
-      <div className="mt-2">
-        <ComboBox options={genderOptions} title="User" />
-      </div>
-    </Drawer>
+    </Modal>
   );
 };
