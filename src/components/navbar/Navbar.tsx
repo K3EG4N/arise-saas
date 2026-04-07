36596194164bf302814f09adf13ac28838a307bc
Felipe Espinoza <j.felipe.espinozah@gmail.com>
2026-04-06 23:03:23 -0500

    cambio momentaneo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,36 @@
-// import { Notifications } from "./fragments/Notifactions";
-import { UserBadge } from "./fragments/UserBadge";
 import { Search } from "./fragments/Searcher";
 import { useLocation } from "react-router-dom";
-import { BreadCrum, Divider } from "arise-ui";
+import { BreadCrum, useRenderIcon } from "arise-ui";
 import { Notifications } from "./fragments/Notifactions";
+import { useState } from "react";
 
 export const Navbar = () => {
   const location = useLocation();
+  const { getIconByName } = useRenderIcon();
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <header className="flex h-fit items-center justify-between gap-4 rounded bg-neutral-50 px-4 py-2 shadow dark:bg-neutral-900">
-      <BreadCrum items={["Main", location?.state?.route]} />
-      <div className="w-[30%]">
-        <Search />
+      <div className="w-fit">
+        <BreadCrum items={["Main", location?.state?.route]} />
       </div>
       <section className="flex h-full min-w-0 items-center gap-3.5">
-        {/* <Theme /> */}
+        <div className="w-80">
+          <Search />
+        </div>
         <Notifications />
-        <Divider orientation="vertical" />
-        <UserBadge />
+        <div
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {
+            getIconByName(
+              "message",
+              "size-5 text-neutral-600 hover:fill-teal-500 cursor-pointer",
+              isHovered,
+            )?.icon
+          }
+        </div>
       </section>
     </header>
   );
